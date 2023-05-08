93f6a0965057816544e695ef6d2396af57d350ab
galmegiz <somethingsjw@gmail.com>
2023-05-08 22:05:58 +0900

    initialization

---
+++ src/test/java/com/jscode/demoApp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jscode.demoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
