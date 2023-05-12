2ca1eaa5d712911b2833f989babf12b053638693
galmegiz <somethingsjw@gmail.com>
2023-05-12 17:32:17 +0900

    feat : 게시글 작성에 대한 유효성 검증 기능 추가

--- src/main/java/com/jscode/demoApp/dto/request/ArticleRequestDto.java
+++ src/main/java/com/jscode/demoApp/dto/request/ArticleRequestDto.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
@@ -14,15 +17,15 @@ import java.time.LocalDateTime;
 @ToString
 public class ArticleRequestDto {
     private Long id;
+    @NotBlank(message = "제목은 필수 입력값입니다.")
+    @Size(min = 1, max = 15, message = "제목은 15자를 넘을 수 없습니다.")
     @Setter
     private String title;
+    @NotNull(message = "내용은 필수 입력값입니다.")
+    @Size(min = 1, max = 1000, message = "내용은 최대 1000자를 넘을 수 없습니다.")
     @Setter
     private String content;
-
     private LocalDateTime createdAt;
-
-    //test용 생성자
-
 
     private ArticleRequestDto(Long id, String title, String content, LocalDateTime createdAt){
         this.id = id;
