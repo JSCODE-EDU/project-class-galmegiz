87192a368688b8af86c4b2f8d18e8e3c8417a902
galmegiz <somethingsjw@gmail.com>
2023-05-17 00:33:43 +0900

    feat : feature#4-3 SearchType이 있는 경우 SearchKeyword 길이 검사하는 CustomValidator 생성

---
+++ src/main/java/com/jscode/demoApp/controller/validator/SearchValidator.java
@@ -0,0 +1,29 @@
+package com.jscode.demoApp.controller.validator;
+
+import com.jscode.demoApp.constant.SearchType;
+import com.jscode.demoApp.dto.request.SearchRequestDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@Slf4j
+public class SearchValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        log.info("customvalidator : {}", clazz.getClass());
+        return SearchRequestDto.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SearchRequestDto searchRequestDto = (SearchRequestDto) target;
+        log.info("customvalidator : {}", searchRequestDto);
+        if(searchRequestDto.getSearchType() != null){
+            if(searchRequestDto.getSearchKeyword().length() < 1){
+                errors.rejectValue("searchKeyword", "Size" ,"검색 키워드는 1글자 이상입니다.");
+            }
+        }
+    }
+}
