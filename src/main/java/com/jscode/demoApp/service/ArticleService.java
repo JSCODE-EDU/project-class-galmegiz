654b2883c4476a55968e504b2f95883f62a50fbd
galmegiz <somethingsjw@gmail.com>
2023-05-28 14:14:48 +0900

    feat : feature#8-1 인증 후 게시판 이용기능으로 변경

--- src/main/java/com/jscode/demoApp/service/ArticleService.java
+++ src/main/java/com/jscode/demoApp/service/ArticleService.java
@@ -2,11 +2,17 @@ package com.jscode.demoApp.service;
 
 import com.jscode.demoApp.constant.SearchType;
 import com.jscode.demoApp.domain.Article;
+import com.jscode.demoApp.domain.Member;
 import com.jscode.demoApp.dto.ArticleDto;
+import com.jscode.demoApp.dto.MemberDto;
+import com.jscode.demoApp.dto.UserPrincipal;
 import com.jscode.demoApp.dto.request.SearchRequestDto;
+import com.jscode.demoApp.dto.response.ErrorResponseDto;
 import com.jscode.demoApp.error.ErrorCode;
+import com.jscode.demoApp.error.exception.AuthorizeException;
 import com.jscode.demoApp.error.exception.ResourceNotFoundException;
 import com.jscode.demoApp.repository.ArticleRepository;
+import com.jscode.demoApp.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +27,7 @@ import java.util.List;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final MemberService memberService;
 
     public ArticleDto getArticle(Long id){
         Article article = articleRepository.findById(id)
@@ -51,32 +58,46 @@ public class ArticleService {
         return articles.stream().map(ArticleDto::fromEntity).toList();
     }
 
-    public ArticleDto createArticle(ArticleDto articleDto){ //메소드명은 save로 통일하는 게 좋은가?
+    public ArticleDto createArticle(ArticleDto articleDto, Long userId){ //메소드명은 save로 통일하는 게 좋은가?
+        Member member = memberService.findMemberById(userId).toEntity();
+
         Article newArticle = Article.builder()
                                     .title(articleDto.getTitle())
                                     .content(articleDto.getContent())
+                                    .member(member)
                                     .build();
+
         articleRepository.save(newArticle);
+
         return ArticleDto.fromEntity(newArticle);
     }
 
-    public void deleteArticle(Long id){
-        //Todo : findById() 메소드 자체에서 값이 없을 경우 EntityNotFoundException을 throw하면 코드 중복을 줄일 수 있을 듯 하다.
-        Article article = articleRepository.findById(id)
-                .orElseThrow(() -> {
-                    throw new ResourceNotFoundException(ErrorCode.ARTICLE_NOT_FOUND);
-                });
+    public void deleteArticle(Long articleId, Long userId){
+        Article article = requestAuthorizeCheck(articleId, userId);
         articleRepository.delete(article);
     }
 
-    public ArticleDto updateArticle(ArticleDto articleDto){
-        Article article = articleRepository.findById(articleDto.getId())
-                .orElseThrow(() -> {
-                    throw new ResourceNotFoundException(ErrorCode.ARTICLE_NOT_FOUND);
-                });
+    public ArticleDto updateArticle(ArticleDto articleDto, Long userId){
+        Article article = requestAuthorizeCheck(articleDto.getId(), userId);
         article.update(articleDto.getTitle(), articleDto.getContent());
         return ArticleDto.fromEntity(article);
     }
+
+    private Article requestAuthorizeCheck(Long articleId, Long userId){
+        MemberDto memberDto = memberService.findMemberById(userId);
+
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> {
+                    throw new ResourceNotFoundException(ErrorCode.ARTICLE_NOT_FOUND);
+                });
+
+        if(article.getMember().getId() != memberDto.getId()){
+            throw new AuthorizeException(ErrorCode.UNAUTHORIZED_RESOURCE_ACCESS);
+        }
+
+        return article;
+    }
+
 
 
 }
